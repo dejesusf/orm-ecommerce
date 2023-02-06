1e51cb444522dc6bb0db993ed2630b04516fed9b
dejesusf <dejesusf@uw.edu>
2023-02-06 00:34:26 -0800

    updated env file

--- models/Category.js
+++ models/Category.js
@@ -7,12 +7,19 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull:false,
+      primaryKey: true,
+    },
+    categoryName: {
+      type: DataTypes.STRING,
+      allowNull:false,
+    },
   },
   {
     sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
     modelName: 'category',
   }
 );
--- models/Product.js
+++ models/Product.js
@@ -10,12 +10,34 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    productName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      }
+    },
   },
   {
     sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
     modelName: 'product',
   }
 );
